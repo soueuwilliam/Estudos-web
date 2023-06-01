8e73dfbdc7c01cd284c859ba5995b7f6e40dfdd6
William campos <39564940+soueuwilliam@users.noreply.github.com>
2023-06-01 09:17:01 -0300

    Finalização Modulo 4

--- Angular/Desenvolvimento_SPA_Angular/MinhaAppAngular/src/app/app.module.ts
+++ Angular/Desenvolvimento_SPA_Angular/MinhaAppAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NomeCoponenteComponent } from './pasta/nome-coponente/nome-coponente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomeCoponenteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/Desenvolvimento_SPA_Angular/MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    //seus componentes
+  ],
+  imports: [
+    CommonModule,
+    //seus Modulos
+  ],
+  providers: [
+    //Seus serviços
+  ],
+})
+export class FuncionalidadeModule {}
